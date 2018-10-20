b493fabc32f532fd88f6e61c8fcdb84b06b2ae0c
Vili <vili.hatonen@helsinki.fi>
2018-10-20 23:15:25 +0300

    WE HAVE A MODEL. Thor is born.

--- data/wrangle_data.py
+++ data/wrangle_data.py
@@ -2,6 +2,8 @@ __author__ = "Vili Hätönen"
 
 import os
 import thor
+import ast
+
 
 name = "speed_data_2.txt"
 data_str = ""
@@ -11,10 +13,16 @@ with open(name, "r") as file:
 
 data = data_str.split("*** NEW DATA SET ***\n")
 data = data[1:]
+for i in range(len(data)):
+    data[i] = ast.literal_eval(data[i])
+    
+print ("data 0", data[0][:10], "data 1",  data[1][:10])
 
 print("should be list type now", type(data))
 print("data length", len(data))
 print("data table length ", len(data[0]))
+print("data table table length ", len(data[0][0]), data[0][0])
+
 
 model = thor.ragnarok(data)
 
