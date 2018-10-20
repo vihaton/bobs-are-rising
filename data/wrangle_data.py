b848abc1a9299cd8908dc818788be49e497874d9
Vili <vili.hatonen@helsinki.fi>
2018-10-21 01:39:51 +0300

    test replaying one ride; it works

--- data/wrangle_data.py
+++ data/wrangle_data.py
@@ -5,7 +5,7 @@ import thor
 import ast
 
 
-name = "speed_data_2.txt"
+name = "speed_data_circle_2.txt"
 data_str = ""
 
 with open(name, "r") as file:
@@ -15,8 +15,8 @@ data = data_str.split("*** NEW DATA SET ***\n")
 data = data[1:]
 for i in range(len(data)):
     data[i] = ast.literal_eval(data[i])
-    
-print ("data 0", data[0][:10], "data 1",  data[1][:10])
+
+#print ("data 0", data[0][:10], "data 1",  data[1][:10])
 
 print("should be list type now", type(data))
 print("data length", len(data))
