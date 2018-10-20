12d8ada9412b96868a627f4bf9483327bb9256df
sofivanhanen <sofia.vanhanen0@gmail.com>
2018-10-20 21:14:59 +0300

    align_values

--- solutions/thor.py
+++ solutions/thor.py
@@ -15,27 +15,38 @@ def transform_source(arrays):
     # Finally return the array of means
     return means
 
-def calculate_means(arrays):
-    # TODO
-    return arrays
+def align_values(arrays):
+    final_arrays = [] # Three-dimensional
+    for array in arrays:
+        new_array = [] # Two-dimensional
+        starting_time = 0
+        for value in array:
+            if (starting_time == 0):
+                if value[1] > 0 or value[2] > 0:
+                    starting_time = value[0]
+            if (starting_time != 0): # no elif in order to handle first case
+                new_array.append([value[0] - starting_time, value[1], value[2]])
+        final_arrays.append(new_array)
+    return final_arrays
 
 def choose_values(arrays):
     # Here we choose which of the measurements to disregard and which to use.
     # We get approximately a few measurements every 0.1 seconds. We create the final array with measurements every 0.05 seconds.
-    final_arrays = []
+    final_arrays = [] # Three-dimensional
     for array in arrays:
-        new_array = []
+        new_array = [] # Two-dimensional
         currentTime = 0
         for value in array:
             if value[0] > currentTime:
-                while (value[0] > currentTime):
+                while (value[0] > currentTime + 50): # Ensure we didn't skip one (possible due to infrequent recordings)
+                    new_array.append(new_array[len(new_array) - 1])
                     currentTime += 50
                 new_measurement = [currentTime, value[1], value[2]]
                 new_array.append(new_measurement)
+                currentTime += 50
         final_arrays.append(new_array)
     return final_arrays
 
-def align_values(arrays):
+def calculate_means(arrays):
     # TODO
-    return []
-        
+    return arrays
