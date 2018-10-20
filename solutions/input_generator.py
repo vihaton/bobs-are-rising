45c6d621e7eb951fc1dc18ae89adcd20363b8dec
sofivanhanen <sofia.vanhanen0@gmail.com>
2018-10-20 21:06:27 +0300

    Improvements to input generator

--- solutions/input_generator.py
+++ solutions/input_generator.py
@@ -6,8 +6,8 @@ import os
 import sys
 import random
 
-NUMBER_OF_ARRAYS = 20
-SIZE_OF_ARRAY = 10000
+NUMBER_OF_ARRAYS = 2
+SIZE_OF_ARRAY = 10
 
 def generate_fake_inputs():
     inputs = []
@@ -20,13 +20,15 @@ def generate_input():
     for i in range(SIZE_OF_ARRAY):
         # TODO fix bad values
         array[i][0] = i + random.uniform(-0.1, 0.1)
-        array[i][1] = (i / SIZE_OF_ARRAY) * 100
-        array[i][2] = (i / SIZE_OF_ARRAY) * 100
-        if (i > SIZE_OF_ARRAY / 2):
-            array[i][1] = 50
+        array[i][1] = 0
+        array[i][2] =  0
+        if (i > SIZE_OF_ARRAY / 5):
+            array[i][1] = 50 + random.uniform(-5, 5)
             array[i][2] = 0
-        elif (i > SIZE_OF_ARRAY - (SIZE_OF_ARRAY / 5)):
-            array[i][1] = 20
-            array[i][2] = 40
-    array = numpy.random.normal(array, 0.1)
+        if (i > SIZE_OF_ARRAY / 2):
+            array[i][1] = (i / SIZE_OF_ARRAY) * 100 + random.uniform(-5, 5)
+            array[i][2] = (i / SIZE_OF_ARRAY) * 100 + random.uniform(-5, 5)
+        if (i > SIZE_OF_ARRAY - (SIZE_OF_ARRAY / 5)):
+            array[i][1] = 20 + random.uniform(-5, 5)
+            array[i][2] = 40 + random.uniform(-5, 5)
     return array
