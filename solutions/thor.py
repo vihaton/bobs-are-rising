df577cfa30d46cedff9f95266df0112912b68d90
sofivanhanen <sofia.vanhanen0@gmail.com>
2018-10-20 22:35:44 +0300

    calculate_means

--- solutions/thor.py
+++ solutions/thor.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
 import numpy
+import os
+import sys
+import builtins
+import generator_test
 
 def transform_source(arrays):
     # First align values to start at the same time
@@ -39,7 +43,12 @@ def choose_values(arrays):
         for value in array:
             if value[0] > currentTime:
                 while (value[0] > currentTime + 50): # Ensure we didn't skip one (possible due to infrequent recordings)
-                    new_array.append(new_array[len(new_array) - 1])
+                    if (currentTime == 0): # Corner case
+                        new_array.append([currentTime, 0, 0])
+                    else:
+                        last_one = new_array[len(new_array) - 1].copy()
+                        last_one[0] = currentTime
+                        new_array.append(last_one)
                     currentTime += 50
                 new_measurement = [currentTime, value[1], value[2]]
                 new_array.append(new_measurement)
@@ -48,5 +57,29 @@ def choose_values(arrays):
     return final_arrays
 
 def calculate_means(arrays):
-    # TODO
-    return arrays
+    final_array = [] # Two-dimensional
+    i = 0
+    while True:
+        time = i * 50
+        mean_1 = 0
+        mean_2 = 0
+        number_of_arrays_handled = 0
+
+        for array in arrays:
+            if (len(array) > i):
+                if array[i][0] != time and time != 0:
+                    raise Exception('Time was not expected: ' + str(array[i][0]))
+                mean_1 += array[i][1]
+                mean_2 += array[i][2]
+                number_of_arrays_handled += 1
+
+        if (number_of_arrays_handled == 0):
+            return final_array
+
+        mean_1 = mean_1 / number_of_arrays_handled
+        mean_2 = mean_2 / number_of_arrays_handled
+        final_array.append([time, mean_1, mean_2])
+
+        i += 1
+
+    return final_array
